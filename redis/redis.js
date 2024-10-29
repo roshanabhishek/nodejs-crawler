6f64aa4a4f7c2b14c6c753f7daa8e7b46ee0ec68
Roshan Abhishek <roshan@eximpe.com>
2024-10-29 19:36:53 +0530

    Clean up redis

--- redis/redis.js
+++ redis/redis.js
@@ -1,13 +1,14 @@
 import { createClient } from 'redis';
 import { redisHostName, redisPassword, redisPostNumber } from '../config/config.js';
 
-const client = createClient({
+const client = await createClient({
     password: redisPassword,
     socket: {
         host: redisHostName,
         port: redisPostNumber,
     }
-});
+}).on('error', err => console.error('Redis Client Error', err))
+.connect();;
 
 // Handle connection events
 client.on('connect', () => {
@@ -20,29 +21,33 @@ client.on('error', (err) => {
 // Function to save string and URL as key-value
 export const saveToRedis = async (key, url) => {
   try {
-    await client.connect();
     // Set key and value in Redis
     await client.set(key, url);
     console.log(`Saved to Redis: ${key} -> ${url}`);
   } catch (error) {
     console.error('Error saving to Redis:', error);
-  } finally {
-    // Close the Redis connection
-    client.quit();
-  }
+  } 
 }
 
 export const getFromRedis = async  (key) =>  {
   try {
-    await client.connect();
     // Get value from Redis
     const value = await client.get(key);
     console.log(`Retrieved from Redis: ${key} -> ${value}`);
     return value;
   } catch (error) {
     console.error('Error retrieving from Redis:', error);
-  } finally {
-    // Close the Redis connection
-    client.quit();
+  }
+}
+
+export const removeFromRedis = async (key) =>  {
+  console.log(key)
+  try {
+    // Get delete from Redis
+    const value = await client.del([`${key}`]);
+    console.log(`Retrieved from Redis: ${key} -> ${value}`);
+    return value;
+  } catch (error) {
+    console.error('Error deleting from Redis:', error);
   }
 }
