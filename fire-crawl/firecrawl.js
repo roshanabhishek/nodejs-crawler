fba291e0e6cb96726019ff90bb92effb5be4c0e0
Roshan Abhishek <roshan@eximpe.com>
2024-10-29 16:16:55 +0530

    Save file to S3 and url to redis

--- fire-crawl/firecrawl.js
+++ fire-crawl/firecrawl.js
@@ -4,12 +4,9 @@ import { firecrawlSecretKey } from '../config/config.js';
 const app = new FirecrawlApp({apiKey: firecrawlSecretKey});
 
 // {
-//     "message": "Scrape started successfully",
-//     "data": {
-//         "success": true,
-//         "id": "2f4535bb-d456-41ac-9a29-ccffd60ae63a",
-//         "url": "https://api.firecrawl.dev/v1/batch/scrape/2f4535bb-d456-41ac-9a29-ccffd60ae63a"
-//     }
+//    "success": true,
+//     "id": "2f4535bb-d456-41ac-9a29-ccffd60ae63a",
+//     "url": "https://api.firecrawl.dev/v1/batch/scrape/2f4535bb-d456-41ac-9a29-ccffd60ae63a"
 // }
 export const startAsyncBatchScrape = async (urls) => {
     const crawlResponse = await app.asyncBatchScrapeUrls(urls, { formats: ['markdown', 'html'] });
--- scrape/scrape.js
+++ scrape/scrape.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { getGoogleApiSearchResult } from "../google-apis/google.js";
 import { checkForAsyncBatchScrapingStatus, startAsyncBatchScrape } from "../fire-crawl/firecrawl.js";
+import { uploadFileToS3 } from "../aws-s3/awsFileUpload.js";
+import { getFromRedis, saveToRedis } from "../redis/redis.js";
 
 const ScrapeRouter = Router();
 
@@ -14,14 +16,24 @@ ScrapeRouter.post("/", async function (req, res) {
         if (!search_text) {
           throw new Error('Search text is required');
         }
-        // Google Search for URLs
-        const searchTextWithKeyWords = getSearchWithKeyWords(search_text);
 
-        const searchResponse = await getGoogleApiSearchResult(searchTextWithKeyWords);
-        const topSearchUrls = searchResponse.items.map(item => item.link);
+        // either batch processing id or file
+        const data = await getFromRedis(search_text);
+        let apiResponseData = null;
+        if(data) {
+          apiResponseData = data
+        } else {
+          const searchTextWithKeyWords = getSearchWithKeyWords(search_text);
 
-        const batchScrapingPostStartedResponse = await startAsyncBatchScrape(topSearchUrls)
-        res.json({ message: 'Scrape started successfully', data: batchScrapingPostStartedResponse });
+          // Google Search for URLs
+          const searchResponse = await getGoogleApiSearchResult(searchTextWithKeyWords);
+          const topSearchUrls = searchResponse.items.map(item => item.link);
+
+          const batchScrapingPostStartedResponse = await startAsyncBatchScrape(topSearchUrls)
+          apiResponseData = batchScrapingPostStartedResponse;
+          saveToRedis(search_text, batchScrapingPostStartedResponse.id)
+        }
+        res.json({ message: 'Scrape started successfully', data: apiResponseData });
       } catch (error) {
         res.status(400).json({ error: error.message });
       }
@@ -29,13 +41,39 @@ ScrapeRouter.post("/", async function (req, res) {
 
 ScrapeRouter.get("/status", async function (req, res) {
   try {
-      const { batch_id } = req.body;
-      if (!batch_id) {
-        throw new Error('Batch ID is required');
+      const { search_text } = req.body;
+      if (!search_text) {
+        throw new Error('search text is required');
+      }
+      let apiResponseData = null;
+
+      // either batch processing id or file
+      const data = await getFromRedis(search_text);
+
+      if(data) {
+        // is a s3 URL
+        if(data.includes('amazonaws.com')) {
+          apiResponseData = data;
+        } else {
+          const statusCheckResponse = await checkForAsyncBatchScrapingStatus(data)
+          if(statusCheckResponse.completed) {
+            const compileMarkDownToSingle = statusCheckResponse.data.map(each => each.markdown).join('\n');
+
+            const responeToS3 = uploadFileToS3(search_text, 'response.txt', JSON.stringify(statusCheckResponse, null, 2))
+            const mdFileToS3 = uploadFileToS3(search_text, 'crawl_data.md', compileMarkDownToSingle)
+            const [_, mdFileUrl] = await Promise.all([responeToS3, mdFileToS3])
+
+            await saveToRedis(search_text, mdFileUrl)
+            apiResponseData = mdFileUrl;
+          } else {
+            apiResponseData = statusCheckResponse;
+          }
+        }
+      } else {
+        throw new Error('search text is not valid');
       }
 
-      const statusCheck = await checkForAsyncBatchScrapingStatus(batch_id)
-      res.json({ message: 'Scrape status', data: statusCheck });
+      res.json({ message: 'Scrape status', data: apiResponseData });
     } catch (error) {
       res.status(400).json({ error: error.message });
     }
